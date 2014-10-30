15593d073d6dbd713734381cec11c9b1fd17e291
Koki Shibata <uryyyyyyy@users.noreply.github.com>
2014-10-30 17:16:22 +0900

    Update log.js
    modify

--- logger/log.js
+++ logger/log.js
@@ -1,15 +1,19 @@
 
-function log (callback) {
+function log () {
 	var Log = require('log')
 	var fs = require('fs')
 	var stream = fs.createWriteStream(__dirname + '/test.log')
-	var log = new Log(Log.WARNING, stream);
-
-	log.debug('a debug message');
-	log.info('a info message');
-	log.warning('a warning message');
-	log.error('a error message');
-	log.emergency('a emergency message');
+	var log = new Log(Log.WARNING, stream);//edit log level
+	
+	return{
+		debug : function(msg){
+			console.log(msg);
+			log.debug(msg);
+		},
+		error : function(msg){
+			log.error(msg);
+		}
+	}
 }
 
 module.exports = log;
