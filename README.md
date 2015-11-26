5ff611183d9c3a44445053f2c934a0ee9d9dbd64
uryyyyyyy <koki305@gmail.com>
2015-11-26 14:32:17 +0900

    readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 nodeJsSamples
 =============
+
+## setup
+
+`npm install`
+
+## run
+
+`node src/<any Dir>/main.js`
+
+## test
+
+`./node_modules/mocha/bin/mocha test/<any Dir>/utilTest.js`
+
--- main.js
+++
@@ -1,9 +0,0 @@
-var mysqlClient = require('./mysqlClient.js');
-var request = require('./request.js');
-var log = require('./log.js');
-
-var logger = function(result){
-	console.log(result);
-};
-
-log(logger);
